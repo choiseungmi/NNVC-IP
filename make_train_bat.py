65b1ad0fe0d5b8c8ea176d2d20dea1eeef101653
최승미 <altmd99@naver.com>
2021-10-23 07:35:01 +0900

    [vtm] train dataset에 맞는 배치 파일 생성

--- make_train_bat.py
+++ make_train_bat.py
@@ -2,24 +2,29 @@ import os
 import sys
 import argparse
 
-def main(file_path, cfg, path_input, path_bitstream, frame_rate):
+def main(sequence_list, i, base_path):
     qp_list = ['22', '27', '32', '37']
     lines = []
 
-    path_bitstream = "output\\decoder\\" + path_bitstream
+    cfg = base_path + "\\bin\\train.cfg"
+    file_path = base_path + "\\bin\\" + i + ".bat"
 
-    for qp in qp_list:
-        path_recon_file = path_input.split(".")[0]+"_qp"+qp+".yuv"
-        path_log = path_input.split(".")[0]+"_qp"+qp+".log"
-        enc_line = "EncoderApp.exe -c encoder_intra_vtm.cfg -c "+cfg+" -i "+path_input+" -b "+path_bitstream+" -q "+qp+" -fr "+frame_rate+" --ReconFile=output\\recon\\"+path_recon_file+" -fs 0 > output\\encoder\\"+path_log+"\n"
-        dec_line = "DecoderApp.exe -b "+path_bitstream+" -o output\\decoder\\"+path_recon_file + " > output\\decoder\\"+path_log+"\n"
-        lines.append(enc_line)
-        lines.append("\n")
-        #lines.append(dec_line)
-        #lines.append("\n")
+    for sequence in sequence_list:
+        path_input = i + "\\" + sequence
+        path_bitstream = "output\\decoder\\" + i + "\\" + sequence.split(".")[0] + ".bin"
 
-    with open(file_path, 'w') as f:
-        f.writelines(lines)
+        for qp in qp_list:
+            path_recon_file = path_input.split(".")[0]+"_qp"+qp+".yuv"
+            path_log = path_input.split(".")[0]+"_qp"+qp+".log"
+            enc_line = "EncoderApp.exe -c encoder_intra_vtm.cfg -c "+cfg+" -i "+path_input+" -b "+path_bitstream+" -q "+qp+" --ReconFile=output\\recon\\"+path_recon_file+" -fs 0 > output\\encoder\\"+path_log+"\n"
+            dec_line = "DecoderApp.exe -b "+path_bitstream+" -o output\\decoder\\"+path_recon_file + " > output\\decoder\\"+path_log+"\n"
+            lines.append(enc_line)
+            lines.append("\n")
+            #lines.append(dec_line)
+            #lines.append("\n")
+
+        with open(file_path, 'w') as f:
+            f.writelines(lines)
 
 
 if __name__ == "__main__":
@@ -33,14 +38,7 @@ if __name__ == "__main__":
             os.makedirs(base_path+"\\bin\\output\\decoder\\"+i)
         if not os.path.exists(base_path+"\\bin\\output\\recon\\"+i):
             os.makedirs(base_path+"\\bin\\output\\recon\\"+i)
-        for j in sequence_list:
-            path_input = i+"\\"+j
-            sequence = j.split("_")[0]
-            file_path = base_path +"\\bin\\"+ i +"_"+ sequence + ".bat"
-            cfg = base_path + "\\cfg\\per-sequence\\" + sequence+".cfg"
-            path_bitstream = i+"\\"+j.split(".")[0]+".bin"
-            frame_rate = j.split("_")[2][:2]
-            frame = str(int(frame_rate) * 2)
+        main(sequence_list, i, base_path)
 
-            main(file_path, cfg, path_input, path_bitstream, frame_rate)
+
 
