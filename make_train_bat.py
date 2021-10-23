a35b8d484a1e41f7bc5b485ff4ae00edf0434055
최승미 <altmd99@naver.com>
2021-10-23 15:29:48 +0900

    [vtm] bat파일에 yuv 변환 코드 추가

--- make_train_bat.py
+++ make_train_bat.py
@@ -1,16 +1,30 @@
 import os
 import sys
 import argparse
+import cv2
+
+
+def convert_rgb2yuv420(input_path):
+    savepath = input_path.split("\\")[:-1] + "_yuv\\"+input_path.split("\\")[-1]
+    print(savepath)
+    # if not os.path.exists(savepath):
+    #     os.makedirs(savepath)
+
+    convert = 'ffmpeg -i '+input_path+' -pix_fmt yuv420p '+savepath
+    # os.system(convert)
 
 def main(sequence_list, i, base_path):
     qp_list = ['22', '27', '32', '37']
     lines = []
 
     cfg = "train.cfg"
-    file_path = base_path + i + ".bat"
+    file_path = base_path + "\\" + i + ".bat"
 
     for sequence in sequence_list:
         path_input = i + "\\" + sequence
+        img = cv2.imread(path_input)
+        h, w, c = img.shape
+        convert_rgb2yuv420(path_input)
 
         for qp in qp_list:
             decoder_path = "output\\decoder\\" + i + "\\" + qp + "\\"
@@ -26,15 +40,15 @@ def main(sequence_list, i, base_path):
 
             path_recon_file = recon_path + sequence+"_qp"+qp+".yuv"
             path_log = encoder_path+sequence+"_qp"+qp+".log"
-            enc_line = "EncoderApp.exe -c encoder_intra_vtm.cfg -c "+cfg+" -i "+path_input+" -b "+path_bitstream+" -q "+qp+" --ReconFile="+path_recon_file+" -fs 0 > "+path_log+"\n"
+            enc_line = "EncoderApp.exe -c encoder_intra_vtm.cfg -c "+cfg+" -i "+path_input+" -b "+path_bitstream+" -w "+str(w) + " -h "+str(h)+" -q "+qp+" --ReconFile="+path_recon_file+" -fs 0 > "+path_log+"\n"
             dec_line = "DecoderApp.exe -b "+path_bitstream+" -o "+path_recon_file + " > output\\decoder\\"+qp+"\\"+path_log+"\n"
             lines.append(enc_line)
             lines.append("\n")
             #lines.append(dec_line)
             #lines.append("\n")
 
-        with open(file_path, 'w') as f:
-            f.writelines(lines)
+    with open(file_path, 'w') as f:
+        f.writelines(lines)
 
 
 if __name__ == "__main__":
