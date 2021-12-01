37642a2cb3f8f6847ff2b1d9e0af19b7fe11a534
choiseungmi <2018102242@khu.ac.kr>
2021-12-01 16:41:26 +0900

    .gitignore is noe working

--- 37.bat
+++
@@ -1,9 +0,0 @@
-set root=C:\Users\user\anaconda3
-call %root%\Scripts\activate.bat %root% 
-
-call conda env list 
-call conda activate cap2
-call cd C:\Users\user\Desktop\VVCSoftware_VTM-VTM-9.0
-call python train.py --epochs 1000 -lr 1e-2 --batch-size 16 -q 37 -hgt 32 -wdt 32 --cuda --save
-
-pause
--- config.py
+++
@@ -1,5 +0,0 @@
-train_numpy_path = "C:\\Users\\user\\Desktop\\VVCSoftware_VTM-VTM-9.0\\data"
-valid_numpy_path = "C:\\Users\\user\\Desktop\\VVCSoftware_VTM-VTM-9.0\\valid_data"
-bin_path = "C:\\Users\\user\\Desktop\\VVCSoftware_VTM-VTM-9.0\\VTM\\bin"
-vtm_path = "C:\\Users\\user\\Desktop\\VVCSoftware_VTM-VTM-9.0\\VTM"
-frames_interval = 8
