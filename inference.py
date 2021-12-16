020e1677242a8a33e0a26f36df7acc8cc52ee3c4
choiseungmi <2018102242@khu.ac.kr>
2021-12-16 18:52:37 +0900

    [Issue #3] inference 준비

--- inference.py
+++ inference.py
@@ -1,141 +1,183 @@
-import math
-
+import argparse
+import random
+import shutil
+import sys
+import time
 import torch
+import torch.nn as nn
+import torch.optim as optim
 from torch.utils.data import DataLoader
-from tqdm import tqdm
-import torch.nn.functional as F
+from torchvision import transforms
+import os
+from torch.utils.tensorboard import SummaryWriter
 
-import torchvision.transforms as transforms
-from make_dataset.load_dataset import AIHUB_INFERENCE
+import config
 
-import transforms as T
-import opts
+from cluster import load_cluster
+from models.tapnn import TextureAdaptivePNN, block_size
+from dataset.load_dataset import TAPNN
+from util import AverageMeter, CSVLogger
 
 
-def test_none(model, data_load, n_clips=2, n_clip_frames=5):
+def parse_args(argv):
+    parser = argparse.ArgumentParser(description="Example training script.")
+    parser.add_argument(
+        "-d", "--dataset", type=str, help="Training dataset"
+    )
+    parser.add_argument(
+        "-e",
+        "--epochs",
+        default=100,
+        type=int,
+        help="Number of epochs (default: %(default)s)",
+    )
+    parser.add_argument(
+        "-lr",
+        "--learning-rate",
+        default=1e-4,
+        type=float,
+        help="Learning rate (default: %(default)s)",
+    )
+    parser.add_argument(
+        "-n",
+        "--num-workers",
+        type=int,
+        default=0,
+        help="Dataloaders threads (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--batch-size", type=int, default=16, help="Batch size (default: %(default)s)"
+    )
+    parser.add_argument(
+        "-q",
+        "--quality",
+        type=int,
+        default=22,
+        help="Quality (default: %(default)s)",
+    )
+    parser.add_argument("--cuda", action="store_true", help="Use cuda")
+    parser.add_argument("--save", action="store_true", help="Save model to disk")
+    parser.add_argument(
+        "--seed", type=float, help="Set random seed for reproducibility"
+    )
+    parser.add_argument('--clusterk', type=int, default=0, help='cluster index (default: 0)')
+    parser.add_argument("--checkpoint", type=str, help="Path to a checkpoint")
+    parser.add_argument("-hgt", "--height", type=int, default=32, help="block height")
+    parser.add_argument("-wdt", "--width", type=int, default=32, help="block width")
+    args = parser.parse_args(argv)
+    return args
+
+def test_epoch(test_dataloader, model, criterion):
     model.eval()
-
-    folder_name = []
-    prediction = []
+    device = next(model.parameters()).device
+    loss = AverageMeter()
     with torch.no_grad():
-        for data, _, name in tqdm(data_load):
-            data = data.cuda()
-            model.clean_activation_buffers()
-            for j in range(n_clips):
-                output = F.log_softmax(model(data[:, :, (n_clip_frames) * (j):(n_clip_frames) * (j + 1)]), dim=1)
-                # a = [[0.7 for i in range(10)] for k in range(len(data))]
-                # b = torch.log(torch.FloatTensor(a)).cuda()
-                # output = output.ge(b)
-            _, pred = torch.max(output, dim=1)
-            minimum, minimum2 = torch.min(output, dim=1)
-            output[:, 0] = minimum - 0.1
-            _2, pred2 = torch.max(output, dim=1)
-            # print("\n", pred)
-            pred = torch.IntTensor(
-                [pred2[i] if pred[i] == 0 and value < math.log(0.7) else pred[i] for i, value in enumerate(_)]).cuda()
-            pred = torch.IntTensor([3 if value < math.log(0.5) else pred[i] for i, value in enumerate(_)]).cuda()
+        for (a, l, y) in test_dataloader:
+            a = a.to(device)
+            l = l.cuda()
+            y = y.cuda()
+            out = model(a, l)
+            out = out.view(y.shape)
 
-            # print(math.log(0.9),_, pred)
-            pred = pred.clamp(max=3)
-            folder_name += name
-            prediction += (pred.tolist())
-    del data
+    del a
+    del l
+    del y
     # GPU memory delete
     torch.cuda.empty_cache()
-    return prediction, folder_name
+    return loss.avg.item()
 
+def main(argv):
+    args = parse_args(argv)
+    if args.seed is not None:
+        torch.manual_seed(args.seed)
+        random.seed(args.seed)
+    ############################################### load dataset ##############################################
+    if args.dataset is None:
+        args.dataset = config.train_numpy_path
+    train_transforms = transforms.Compose(
+         [transforms.ToTensor()]  # numpy이미지에서 torch이미지로 변경
+    )
+    test_transforms = transforms.Compose(
+        [transforms.ToTensor()]
+    )
+    path_model_cluster = os.path.join(config.cluster_checkpoint, str(args.height)+"x"+str(args.width) + "_" + str(args.quality) + '.pkl')
+    km = load_cluster(path_model_cluster)
 
-def inference_none(model, path, target):
-    Bs_Test = 16
-    transform_test = transforms.Compose([
-        T.ToFloatTensorInZeroOne(),
-        T.Resize((200, 200)),
-        # T.Normalize(mean=[0.43216, 0.394666, 0.37645], std=[0.22803, 0.22145, 0.216989]),
-        T.CenterCrop((172, 172))])
+    test_dataset = TAPNN(os.path.join(config.valid_numpy_path, str(args.quality), str(args.height)+"x"+str(args.width)), args.height, args.width,
+                         transform=test_transforms, km=km, k=args.clusterk)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(torch.cuda.is_available())
+    print(device)
+    test_dataloader = DataLoader(
+        test_dataset,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        shuffle=False,
+        pin_memory=(device == "cuda"),
+    )
 
-    hmdb51_test = AIHUB_INFERENCE(path, transform=transform_test)
-    test_loader = DataLoader(hmdb51_test, batch_size=opts.BATCH_SIZE, shuffle=False)
+    ############################################## model #####################################################
+    h = args.height
+    w = args.width
 
-    prediction, video_list = test_none(model, test_loader)
-    label = ['scratch', 'shake', 'turn', 'none']
+    model = TextureAdaptivePNN(False, h, w)
+    model.cuda()
 
-    total = 0
-    total_label = 0
-    total_none = 0
-    label_acc = 0
-    none_acc = 0
+    criterion = nn.L1Loss()
 
-    for i, pred_idx in enumerate(prediction):
-        if i % 5 == 0: print()
-        print('{:<30s}:{:<15s}'.format(video_list[i], label[pred_idx]), end='|')
-        total += 1
-        if (target == label[pred_idx]): label_acc += 1
-        if not (target in label):
-            if (pred_idx == 3): none_acc += 1
-            total_none += 1
-        else:
-            total_label += 1
-    print("\n\nlabel: ", target, ", total number: ", total)
-    print('total Accuracy:' + '{:5}'.format(label_acc + none_acc) + '/' +
-          '{:5}'.format(total) + ' (' +
-          '{:4.2f}'.format(100 * (label_acc + none_acc) / total) + '%)')
-    print('label Accuracy:' + '{:5}'.format(label_acc) + '/' +
-          '{:5}'.format(total) + ' (' +
-          '{:4.2f}'.format(100 * label_acc / total) + '%)')
-    print('none Accuracy:' + '{:5}'.format(none_acc) + '/' +
-          '{:5}'.format(total) + ' (' +
-          '{:4.2f}'.format(100 * none_acc / total) + '%)')
-    del model
-    del hmdb51_test
+    filename = "log_csv\\"+str(args.height)+"x"+str(args.width)+"_" + str(args.quality) +"_" + str(args.clusterk) + ".csv"
+    csv_logger = CSVLogger(
+        fieldnames=['epoch', 'train_loss', 'test_loss'],
+        filename=filename)
+    writer = SummaryWriter()
 
+    last_epoch = 0
+    if args.checkpoint:  # load from previous checkpoint
+        print("Loading", args.checkpoint)
+        checkpoint = torch.load(args.checkpoint, map_location=device)
+        last_epoch = checkpoint["epoch"] + 1
+        model.load_state_dict(checkpoint["state_dict"])
 
-def test_user(model, data_load, n_clips=2, n_clip_frames=5):
-    model.eval()
+    best_loss = float("inf")
+    for epoch in range(last_epoch, args.epochs):
+        start = time.time()
+        print(f"Learning rate: {optimizer.param_groups[0]['lr']}")
+        train_loss = train_one_epoch(
+            model,
+            criterion,
+            train_dataloader,
+            optimizer,
+            epoch,
+        )
+        loss = test_epoch(epoch, test_dataloader, model, criterion)
+        lr_scheduler.step(loss)
+        print(f"Train epoch {epoch}: "
+                f'\tTrain Loss: {train_loss:.3f}'
+              f'\tTest Loss: {loss:.3f}')
 
-    folder_name = []
-    prediction = []
-    with torch.no_grad():
-        for data, _, name in tqdm(data_load):
-            data = data.cuda()
-            model.clean_activation_buffers()
-            for j in range(n_clips):
-                output = F.log_softmax(model(data[:, :, (n_clip_frames) * (j):(n_clip_frames) * (j + 1)]), dim=1)
-                # a = [[0.7 for i in range(10)] for k in range(len(data))]
-                # b = torch.log(torch.FloatTensor(a)).cuda()
-                # output = output.ge(b)
-            _, pred = torch.max(output, dim=1)
-            folder_name += name
-            prediction += (pred.tolist())
-    del data
-    # GPU memory delete
-    torch.cuda.empty_cache()
-    return prediction, folder_name
+        writer.add_scalars("Loss_"+str(args.quality)+"_"+str(args.clusterk), {'train': train_loss,
+                                    'valid': loss}, epoch)
 
+        row = {'epoch': str(epoch), 'train_loss': str(train_loss), 'test_loss': str(loss)}
+        csv_logger.writerow(row)  ###
+        is_best = loss < best_loss
+        best_loss = min(loss, best_loss)
 
-def inference_user(model, path, target):
-    transform_test = transforms.Compose([
-        T.ToFloatTensorInZeroOne(),
-        T.Resize((200, 200)),
-        # T.Normalize(mean=[0.43216, 0.394666, 0.37645], std=[0.22803, 0.22145, 0.216989]),
-        T.CenterCrop((172, 172))])
+        if args.save:
+            save_checkpoint(
+                {
+                    "epoch": epoch,
+                    "state_dict": model.state_dict(),
+                    "loss": loss,
+                    "optimizer": optimizer.state_dict(),
+                    "lr_scheduler": lr_scheduler.state_dict(),
+                },
+                is_best,
+                str(args.quality), str(args.height), str(args.width), str(args.clusterk)
+            )
+        print(f"Total TIme: {time.time() - start}")
+    csv_logger.close()  ###
+    writer.flush()
 
-    hmdb51_test = AIHUB_INFERENCE(path, transform=transform_test)
-    test_loader = DataLoader(hmdb51_test, batch_size=opts.BATCH_SIZE, shuffle=False)
-
-    prediction, video_list = test_user(model, test_loader)
-    label = ['bite', 'eat', 'jump', 'lie', 'scratch', 'shake', 'sit', 'sniff', 'stand', 'turn', 'walk']
-
-    total = 0
-    acc = 0
-
-    for i, pred_idx in enumerate(prediction):
-        if i % 5 == 0: print()
-        print('{:<30s}:{:<15s}'.format(video_list[i], label[pred_idx]), end='|')
-        total += 1
-        if (target == label[pred_idx]): acc += 1
-    print("\n\nlabel: ", target, ", total number: ", total)
-    print('Accuracy:' + '{:5}'.format(acc) + '/' +
-          '{:5}'.format(total) + ' (' +
-          '{:4.2f}'.format(100 * acc / total) + '%)\n')
-    del model
-    del hmdb51_test
+if __name__ == "__main__":
+    main(sys.argv[1:])
